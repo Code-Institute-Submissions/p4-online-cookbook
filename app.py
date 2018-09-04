ce6a7aaca100a997bafb9a1f15fc61979169677b
Sarah Loh <sarah.loh0708@gmail.com>
2018-09-04 12:16:20 +0100

    Connect app to mongodb and add templates

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'sl_task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello Online Cookbook'
+@app.route('/get_recipes')
+def get_recipes():
+    """
+    Display the recipes colection on index.html
+    """
+    return render_template("index.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
